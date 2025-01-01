b9c25c7041a60797c9b04566a4ba0d15acf25c4c
manojLovesCoding <mkum9417@gmail.com>
2025-01-01 15:01:44 +0530

    a code to home component with scss and used axios for API call

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from "react"
+import MovieListing from "../MovieListing/MovieListing.jsx"
+import movieApi from "../../common/apis/movieApi.js";
+import { APIKey } from "../../common/apis/MovieApiKey.js";
 const Home = () => {
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log("Err :",err);
+        });
+      console.log("The response from API ",response);
+    };
+    fetchMovies()
+  },[]);
   return (
-    <div>Home</div>
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing/>
+    </div>
   )
 }
 
