29147275dcb22e903e915ae1cc6c213db3ac5335
manojLovesCoding <mkum9417@gmail.com>
2025-01-05 17:17:37 +0530

    displayed Movie Card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,21 @@
-const MovieCard = () => {
+import "./MovieCard.scss";
+const MovieCard = (props) => {
+  const { data } = props;
   return (
-    <div>MovieCard</div>
-  )
-}
+    <div className="card-item">
+      <div className="card-inner">
+        <div className="card-top">
+          <img src={data.Poster} alt={data.Title} />
+        </div>
+        <div className="card-bottom">
+          <div className="card-info">
+            <h4>{data.Title}</h4>
+            <p>{data.Year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
